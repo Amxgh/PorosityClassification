6a144e81e0cbefa21083090eac5d1761605716a8
Amogh <axm0754@mavs.uta.edu>
2025-01-21 21:13:25 -0600

    ref: cleaned up code

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
-
 # Load a thermal image in grayscale
 image_path = "image.png"
 image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
@@ -22,14 +21,13 @@ resized_image = cv2.resize(normalized_image, (128, 128))
 
 # ! DATA AUGMENTATION
 datagen = ImageDataGenerator(
-    rotation_range=20,       # Random rotation
-    width_shift_range=0.1,   # Horizontal shift
+    rotation_range=20,  # Random rotation
+    width_shift_range=0.1,  # Horizontal shift
     height_shift_range=0.1,  # Vertical shift
-    zoom_range=0.2,          # Zoom in/out
-    horizontal_flip=True,    # Flip horizontally
-    fill_mode='nearest'      # Fill in missing pixels
+    zoom_range=0.2,  # Zoom in/out
+    horizontal_flip=True,  # Flip horizontally
+    fill_mode='nearest'  # Fill in missing pixels
 )
-
 
 # Augmenting the image
 image_batch = resized_image.reshape((1, 128, 128, 1))
@@ -39,5 +37,5 @@ augmented = datagen.flow(image_batch, batch_size=1)
 for i in range(5):
     aug_image = next(augmented)[0].reshape(128, 128)
     plt.imshow(aug_image, cmap='hot')
-    plt.title(f"Augmented Image {i+1}")
+    plt.title(f"Augmented Image {i + 1}")
     plt.show()
