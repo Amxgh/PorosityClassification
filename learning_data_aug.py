de331ca25ef3f785d0821429e00722775cb99efb
Amogh <axm0754@mavs.uta.edu>
2025-01-21 21:16:09 -0600

    ref: renamed file

--- learning_data_aug.py
+++ learning_data_aug.py
@@ -4,7 +4,7 @@ import numpy as np
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 # Load a thermal image in grayscale
-image_path = "dataset/correct/image.png"
+image_path = "image.png"
 image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
 
 # Display the image
