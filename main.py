a75e021abfec28fa82a0247d5f9619ef2b68cf85
Amogh <axm0754@mavs.uta.edu>
2025-03-14 14:26:55 +0100

    Update training script call in main.py to use train_test_model.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 def main():
 
     # os.system("python utilities/data_preprocessing.py")
-    os.system("python utilities/train_model.py")
+    os.system("python utilities/train_test_model.py")
     # os.system("python utilities/evaluate_model.py")
 
 
