3746275367f227515f087e949337c5459c1aef23
Amogh <axm0754@mavs.uta.edu>
2025-05-03 13:22:46 -0500

    Clean up dataset filtering script: remove commented-out code and update augmented sample generation

--- utilities/dataset_filtering.py
+++ utilities/dataset_filtering.py
@@ -5,28 +5,6 @@ import pandas as pd
 import cv2
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
-#
-#
-# filename: str = "./dataverse_files/thermal-porosity-table.csv"
-#
-# # initializing the titles and rows list
-# fields = []
-# rows = []
-#
-# # reading csv file
-# with open(filename, 'r') as csvfile:
-#     # creating a csv reader object
-#     csvreader = csv.reader(csvfile)
-#
-#     # extracting field names through first row
-#     fields = next(csvreader)
-#
-#     # extracting each data row one by one
-#     for row in csvreader:
-#         rows.append(row)
-#
-#     # get total number of rows
-#     print("Total no. of rows: %d" % (csvreader.line_num))
 
 directory = "./data/"
 
@@ -67,7 +45,7 @@ for (root, dirs, files) in os.walk(directory, topdown=True):
             X_values.append(batch[0].astype(np.float32))  # Extract augmented data
             y_values.append(y_value)
             i += 1
-            if i % 15 == 0:  # Generate 7 augmented samples
+            if i % 15 == 0:  # Generate 15 augmented samples
                 break
 
 # Resize each image to (128, 128)
@@ -79,33 +57,3 @@ X_values = X_resized / np.max(np.abs(X_resized))
 
 # Save new dataset
 np.savez("./data/augmented_data.npz", X_values=X_values, y_values=y_values)
-
-# X_values = np.vstack(X_values)
-#
-#
-# np.savez("./data/data.npz",
-#         X_values=X_values,
-#          y_values=y_values)
-
-
-#
-# Y_values: list = [int(row[10]) for row in rows]
-# X_values = []
-#
-# files = [f for f in os.listdir(directory)]
-#
-# files.sort(key=lambda x: int(x.split('_')[1]))
-#
-# for file in files:
-#     file_path = os.path.join(directory, file)
-#
-#     try:
-#         df = pd.read_csv(file_path).iloc[:, 1:] # Drops the index column
-#
-#         if df.shape != (200, 201):
-#             print(df.shape)
-#         X_values.append(df.to_numpy())
-#     except Exception as e:
-#         print(f"Error reading {file}: {e}")
-#
-#
